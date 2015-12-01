ff00365e7a64652f53e837e98046390faa747df4
Mijail Paz <mijail@gmail.com>
2015-11-30 19:25:42 -0600

    upload file
    upload file, get orientation

--- upload.php
+++ upload.php
@@ -1,32 +1,6 @@
 <?php
 set_time_limit(120);
 
-/*
-function resample($jpgFile, $thumbFile, $width, $orientation) {
-    // Get new dimensions
-    list($width_orig, $height_orig) = getimagesize($jpgFile);
-    $height = (int) (($width / $width_orig) * $height_orig);
-    // Resample
-    $image_p = imagecreatetruecolor($width, $height);
-    $image   = imagecreatefromjpeg($jpgFile);
-    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
-    // Fix Orientation
-    switch($orientation) {
-        case 3:
-            $image_p = imagerotate($image_p, 180, 0);
-            break;
-        case 6:
-            $image_p = imagerotate($image_p, -90, 0);
-            break;
-        case 8:
-            $image_p = imagerotate($image_p, 90, 0);
-            break;
-    }
-    // Output
-    imagejpeg($image_p, $thumbFile, 90);
-}
-*/
-
 ini_set('upload_max_filesize', '10M');
 $data = array();
 header('Content-Type: application/json');
